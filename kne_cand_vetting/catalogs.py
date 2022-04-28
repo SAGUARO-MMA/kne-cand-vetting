0bbd6ad5a48911e6628dcc3dd5cc337433c4437c
Jillian Rastinejad <JillyR@dhcp-10-134-244-75.uawifi.arizona.edu>
2022-04-28 09:41:32 -0700

    Updating setup.py, requirements.txt, catalogs.py

--- kne_cand_vetting/catalogs.py
+++ kne_cand_vetting/catalogs.py
@@ -1,87 +1,165 @@
-import numpy as np
-from astropy import units as u
+#!/usr/bin/env python3
+
+
+# +
+# import(s)
+# -
+from astropy.io import ascii
+from sassy_src.models.milliquas_q3c_orm import *
+from sassy_src.models.milliquas_q3c_orm_cli import *
+from sassy_src.models.milliquas_q3c_orm_filters import *
+# from sassy_src.models.asassn_q3c_orm import *
+# from sassy_src.models.asassn_q3c_orm_filters import *
+# from sassy_src.models.asassn_q3c_orm_cli import *
+from typing import Optional
 from astropy.coordinates import SkyCoord
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Float, union, text, and_
-from sqlalchemy.sql import select
-from astropy.io import fits, ascii
-import time, os, sys
+from astropy import units as u
 
-import sqlalchemy
-print(sqlalchemy.__version__)
+import argparse
+import os
+import time
 
-# ssh sassy2
-# PGPASSWORD=SASSy_520 psql --echo-all -h localhost -p 5432 -U sassy -d sassy -c "SELECT * FROM milliquas_q3c;"
 
-def milliquas_query(file,rad=2):
+# +
+# __doc__
+# -
+__doc__ = """ PYTHONPATH=/home/phil_daly/SASSyII python3 catalogs.py --help """
 
-    '''
-    Query the Million Quasar Catalog (Flesch 2021) for matches to
-    kilonova candidates
 
-    PARAMETERS
-    ----------
-    ra, dec : float --> right now it's an ascii file that I pull from
-        degrees
-    radius : float
-        default = 2, arcseconds
+# +
+# constant(s)
+# -
+DB_CONNECT = f"postgresql+psycopg2://sassy:SASSy_520@localhost:5432/sassy"
+FILE = os.path.abspath(os.path.expanduser("comb_master_FINAL.dat"))
+RADIUS_ARCSEC = 2.0
 
-    RETURNS
-    -------
-    match : float
-        1. = match to milliquas source with qpct > 97; 0. = no match
 
-    '''
-    # set up parameters to connect to sassy database where milliquas_q3c table stored
-    dbname = 'sassy'
-    eng = 'postgresql://sassy:SASSy_520@localhost:5432/' + dbname
+# +
+# function: milliquas_query()
+# -
+#  ->  allows you to optionall accept inputs as lists
+def static_cats_query(_file: str = FILE, _radius: float = RADIUS_ARCSEC, _verbose: bool = False) -> Optional[list]:
+    """ Query the Million Quasar Catalog (Flesch 2021) for matches to kilonova candidates """
 
-    engine = create_engine(eng, echo = True)
-    meta = MetaData()
+    # check input(s)
+    _file = os.path.abspath(os.path.expanduser(f"{_file}"))
+    if not os.path.exists(_file):
+        raise Exception(f"invalid input, _file={_file}")
 
-    conn = engine.connect()
+    _radius /= 3600.0 # convert to degrees
+    if _radius <= 0.0:
+        raise Exception(f"invalid input, _radius={_radius}")
 
-    # set up what the columns will look like
-    milliquas = Table(
-    'milliquas_q3c', meta,
-    Column('ra', Float),
-    Column('dec', Float),
-    Column('qpct',Float)
-    )
+    # read the file into an astropy ascii table
+    _candidates = None
+    try:
+        _candidates = ascii.read(_file)
+    except Exception as _e1:
+        if _verbose:
+            print(f"{_e1}")
+        raise Exception(f"Unable to read {_file}")
+    else:
+        if len(_candidates) <= 0:
+            raise Exception(f"File {_file} contains no data")
 
-    all_cand = ascii.read(file)
-    tot_names = all_cand['Name']
-    tot_radeg = all_cand['RA_deg'].astype(float)
-    tot_decdeg = all_cand['Dec_deg'].astype(float)
+    # connect to database
+    try:
+        engine = create_engine(DB_CONNECT)
+        get_session = sessionmaker(bind=engine)
+        session = get_session()
+    except Exception as _e2:
+        if _verbose:
+            print(f"{_e2}")
+        raise Exception(f"Failed to connect to database")
 
-    t0 = time.time()
-    qso = []
-    for i in range(len(tot_radeg)):
-        ra = tot_radeg[i]
-        dec = tot_decdeg[i]
+    _names = [_ for _ in _candidates['Name']]
 
-        # need to make conditions but now this should work!!
-        ra_high = ra + (rad / 3600.)
-        ra_low = ra - (rad / 3600.)
-        dec_high = dec + (rad / 3600.)
-        dec_low = dec - (rad / 3600.)
+    # gather co-ordinates into (Ra, Dec) tuples
+    _coords = tuple(zip(_candidates['RA_deg'], _candidates['Dec_deg']))
 
-        # set conditions to select match
-        s = milliquas.select().where(and_(text("milliquas_q3c.ra between :x1 and :x2"),text("milliquas_q3c.dec between :y1 and :y2"),text("milliquas_q3c.qpct>97.")))
+    # Pass to milliquas_query
+    qso, qprob, qoffset = milliquas_query(session, _coords, _names, _radius)
+    print(qso,qprob,qoffset)
+    session.close()
 
-        result = conn.execute(s, x1 = ra_low, x2 = ra_high, y1 = dec_low, y2 = dec_high).fetchall()
+def milliquas_query(session, coords, names, _radius, _verbose: bool = True):
 
-        if len(result) > 0:
-            print('QUASAR MATCH!')
-            qso.append(1.0)
+    # set variable(s)
+    _begin = time.time()
+
+    qso = []; qoffset = []; qprob = []
+
+    # for all (RA, Dec) tuples, execute cone search and log candidates with qpct > 97(%)
+    for _i, _e in enumerate(coords):
+
+        # set up query
+        try:
+            # _radius /= 99999999999999
+            query = session.query(MilliQuasQ3cRecord)
+            query = milliquas_q3c_orm_filters(query, {'cone': f'{_e[0]},{_e[1]},{_radius}', 'q__gte': 97})
+        except Exception as _e3:
+            if _verbose:
+                print(f"{_e3}")
+            print(f"Failed to execute query for RA, Dec = ({_e[0]}, {_e[1]}), index={_i}")
+            continue
+
+        # execute the query
+        if len(query.all()) == 0:
+            qso.append(None)
+            qprob.append(0.0)
+            qoffset.append(None)
         else:
-            print('No quasar match.')
-            qso.append(0.0)
+            for _x in MilliQuasQ3cRecord.serialize_list(query.all()):
+                print(f'>>> QUASAR MATCH at RA, Dec = ({_e[0]},{_e[1]}), index={_i}!')
 
-    print("Completed in {:.3f} sec".format(time.time()-t0))
-    print("Found {:.1f} QSO of {:.2f} candidates".format(sum(qso),len(qso)))
+                # add the query dictionary to the qso list and modify the qprob list
+                qso.append(_x['name']) #{**_x, **{'Candidate': names[_i], 'Probability': 1.0, 'Candidate_RA': _e[0], 'Candidate_Dec': _e[1]}})
+                qprob.append(1.0)
 
-milliquas_query('comb_master_FINAL.dat')
+                QSO = SkyCoord(_x['ra']*u.deg, _x['dec']*u.deg)
+                cand = SkyCoord(_e[0]*u.deg, _e[1]*u.deg)
+                qoffset.append(cand.separation(QSO).arcsec)
 
+    # done
+    _end = time.time()
+
+    # return list of probabilities (although I think you'd be better off returning the qso list!)
+    # print(f"QSO Candidates: {qso}")
+    # print(f"QSO Probabilities: {qprob}")
+    print(f"Completed in {_end-_begin:.3f} sec")
+    print(f"Found {len(qso)} QSOs in {len(coords)} candidates")
+
+    return qso, qprob, qoffset
+
+def asassn_query(session, coords, RADIUS_ARCSEC, _verbose: bool = False):
+
+    _begin = time.time()
+
+    f_asassn = 0.0 * len(coords)
+
+
+# +
+# main()
+# -
+if __name__ == '__main__':
+
+    # get command line argument(s)
+    _p = argparse.ArgumentParser(description=f'catalogs.py', formatter_class=argparse.RawTextHelpFormatter)
+    _p.add_argument(f'--file', default=FILE, help="""File, default '%(default)s'""")
+    _p.add_argument(f'--radius', default=RADIUS_ARCSEC, help="""Radius (arcsec), default %(default)s""")
+    _p.add_argument(f'--verbose', default=False, action='store_true', help=f'if present, produce more verbose output')
+    _a = _p.parse_args()
+
+    # execute
+    try:
+        static_cats_query(_file=_a.file, _radius=float(_a.radius), _verbose=bool(_a.verbose))
+    except Exception as _x:
+        print(f"{_x}")
+        print(f"Use:{__doc__}")
+
+
+
+### DOES NOT WORK
 def gaia_query(ra,dec,rad=2):
     '''
     Query Gaia eDR3 for a variable star match
