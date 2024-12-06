393010e2c904d27e4957dd60f965cf4f0baf3efd
Noah Franz <noahfranz13@gmail.com>
2024-12-06 08:44:06 -0700

    add redundancy check to make sure code doesn't crash on time limit -99

--- kne_cand_vetting/survey_phot.py
+++ kne_cand_vetting/survey_phot.py
@@ -387,8 +387,11 @@ def TNS_get(get_obj, BOT_ID: str = None, BOT_NAME: str = None, API_KEY: str = No
         if response.status_code != 200:
             return response, -99 # I'm just setting the time_to_reset to -99 so other code doesn't break
         
-        remaining_str = response.headers.get('x-rate-limit-remaining')
+        remaining_str = response.headers.get('x-rate-limit-remaining', -99)
         time_to_reset = int(response.headers.get('x-rate-limit-reset', -99)) # in seconds
+
+        if remaining_str < 0 or time_to_reset < 0:
+            return response, time_to_reset
         
         if remaining_str == 'Exceeded':
             # we already exceeded the rate limit
